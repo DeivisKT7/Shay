8a5d3fbbda22f3db5994100737db234be28bceaf
DeivisKT7 <88854045+DeivisKT7@users.noreply.github.com>
2022-05-10 13:03:29 -1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let clave = document.querySelector(".clave");
+let boton = document.querySelector(".boton");
+
+boton.addEventListener("click", e =>{
+
+	if(clave.value == "7"){
+	   alert("I appreciate you");
+	}else{
+		alert("Error");
+	}
+
+}) 
